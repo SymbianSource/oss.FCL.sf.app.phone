c80365e41623394c6f9e9a64e99f4a5e4e04c23a
Tom Pritchard <tomp@symbian.org>
2010-08-17 18:45:21 +0100

    Patch for BUG 3596 -  Phone app crashes when dialling a number from 3.0.1 onwards
    --HG--
    branch : RCL_3

--- phoneengine/phonemodel/src/cpemessagehandler.cpp
+++ phoneengine/phonemodel/src/cpemessagehandler.cpp
@@ -2443,8 +2443,10 @@ TInt CPEMessageHandler::HandleEmergencyCheck( )
         {
         iModel.SendMessage( MEngineMonitor::EPEMessageInValidEmergencyNumber );
         }
-
-    return errorCode;
+    // CPhCltEmergencyCall::IsEmergencyPhoneNumber can legitamately return an error code
+    // This simply means that the number is NOT an emergency number (eg it can return KErrArgument)
+    // Therefore we are ignoring any KErrArgument errors that come from this
+    return (errorCode == KErrArgument) ? ECCPErrorNone : errorCode;
     }
 
 // -----------------------------------------------------------------------------
